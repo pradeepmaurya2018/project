00d141b36334cc98aa0aa80984f62aa23611c425
Pradeep Maurya <pradeep.maurya2018@gmail.com>
2019-02-25 06:32:15 +0530

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Pradeep Maurya on 25/02/19.
+//  Copyright © 2019 pradeep maurya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
