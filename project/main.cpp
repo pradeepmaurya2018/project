8e09fb3877f17b110d7fc3e8294a65f0b3328076
Pradeep Maurya <pradeep.maurya2018@gmail.com>
2019-02-25 07:53:28 +0530

    you

--- project/main.cpp
+++ project/main.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
 using namespace std;
-class A
-{
-    
-};
-class B
-{
-    
-};
-
-
 
 int main(int argc, const char * argv[]) {
     double val;
     int data;
     double dataType;
     float integration;
-    
     const int g=56;
-    
     std::cout << "Hello, World!\n";
+    int da;
+    int Array[5]={0};
     return 0;
 }
